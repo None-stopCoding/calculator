95c338bea8a7d8fc13c03fff754245d83442fb7b
alex <gkvalex7@gmail.com>
2019-09-26 21:08:44 +0300

    restructure

--- web_calculator/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from web_calculator import views
-
-urlpatterns = [
-    path('', views.input_exp, name='input'),
-]
--- web_calculator/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def input_exp(request):
-    return render(request, 'index.html', {})
